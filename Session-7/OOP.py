992a5a9b90caa7245d51e3e14a0508e76385216e
adem kalfalı <ademkalfa9@gmail.com>
2022-12-17 14:07:54 +0300

    special methods

--- Session-7/OOP.py
+++ Session-7/OOP.py
@@ -46,35 +46,59 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 #! SELF keyword and methods
 
+# class Person:
+#    company = "clarusway"
+
+#    def test(self):
+#        print("test")
+
+#    def set_details(self,name, age):
+#        self.name = name
+#        self.age = age
+
+#    def get_details(self):
+#        print(f"{self.name} - {self.age}")
+
+#    @staticmethod  #! static her instance için aynı sonucu döndürür. 
+#    def salute ():
+#        print("Hi there!")
+
+# person1 = Person()
+# person2 = Person() 
+
+# # person1.test()
+# # Person.test(person1)
+
+
+
+# person1.name= "adam"
+# person1.age = 33
+# person1.get_details()
+
+# person2.name= "henry"
+# person2.age= 18
+
+# person2.set_details("json",15)
+# person2.get_details()
+
+# person1.salute()
+
+#! special methods (dunder methods)
+
 class Person:
-   company = "clarusway"
+    company = "clarusway"
 
-   def test(self):
-       print("test")
+    #automaticaly runs when the instance is created
+    def __init__(self,name, age, gender="male"):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
-   def set_details(self,name, age):
-       self.name = name
-       self.age = age
+    def get_details(self):
+        print(f"{self.name} - {self.age} - {self.gender}")
 
-   def get_details(self):
-       print(f"{self.name} - {self.age}")
-
-person1 = Person()
-person2 = Person() 
-
-# person1.test()
-# Person.test(person1)
-
-
-
-person1.name= "adam"
-person1.age = 33
+person1 = Person("adam", 33)
 person1.get_details()
 
-person2.name= "henry"
-person2.age= 18
-
-person2.set_details("json",15)
-person2.get_details()
-
+person2 = Person("Jason", 22)
 
